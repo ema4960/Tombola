5ca816a664f5a8726fa5c60f87f27aff88847e00
emmanuel <85496078+ema4960@users.noreply.github.com>
2021-11-27 11:21:02 +0100

    update

--- Tombola/script.js
+++ Tombola/script.js
@@ -1,6 +1,12 @@
 function showCartelle(){
     cardsGridBox=document.getElementsByClassName("cards__grid"); 
+    numbersGridBox=document.getElementsByClassName("numbers__grid"); 
     for(i=1;i<=24;i++){
        cardsGridBox[0].innerHTML+="<input type='checkbox'><p>"+i+"</p></input>";
     }
+    for(i=1;i<=90;i++){
+        numbersGridBox[0].innerHTML+="<p id='num"+i+"'>"+i+"</p>";
+    }
+
 }
+
