14387b8c9a7deafe7a6391f2fd5756bfe9a9795c
emmanuel <85496078+ema4960@users.noreply.github.com>
2021-11-28 18:05:20 +0100

    estrazione

--- Tombola/script.js
+++ Tombola/script.js
@@ -1,10 +1,11 @@
 extracted = [];
+SelectedCardS= 0;
 
 function showCartelle(){
     cardsGridBox=document.getElementsByClassName("cards__grid"); 
     numbersGridBox=document.getElementsByClassName("numbers__grid"); 
     for(i=1;i<=24;i++){
-       cardsGridBox[0].innerHTML+="<input type='checkbox'><p>"+i+"</p></input>";
+       cardsGridBox[0].innerHTML+="<input type='checkbox' id='cartella"+i+"'><p>"+i+"</p></input>";
     }
     for(i=1;i<=90;i++){
         numbersGridBox[0].innerHTML+="<p class='num'>"+i+"</p>";
@@ -17,18 +18,18 @@ function extraction(){
     extract3Obj=document.getElementById("extract3");
     extract4Obj=document.getElementById("extract4");
     
-    nExtract=Math.floor(Math.random() * 90);
+    nExtract=Math.floor(Math.random() * 91);
     if(extracted.includes(nExtract)){
         extraction();
     }else{
         extractedObj=document.getElementsByClassName("num");
         extractedObj[nExtract-1].style.backgroundColor="var(--primary500)";
-        extractedObj[nExtract-1].style.color="var(--primary800)"
+        extractedObj[nExtract-1].style.color="var(--primary800)";
         extract4Obj.innerHTML=extract3Obj.innerHTML;
         extract3Obj.innerHTML=extract2Obj.innerHTML;
         extract2Obj.innerHTML=extractObj.innerHTML;
         extractObj.innerHTML=nExtract;
-        extracteded.push(nExtract);
+        extracted.push(nExtract);
     }
 }
 
